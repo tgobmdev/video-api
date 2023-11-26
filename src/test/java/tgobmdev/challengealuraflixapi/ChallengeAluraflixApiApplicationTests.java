7b6f388b80a3767c7001b7a8c85aea3f16906cac
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-26 15:10:02 -0300

    initial commit

---
+++ src/test/java/tgobmdev/challengealuraflixapi/ChallengeAluraflixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tgobmdev.challengealuraflixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAluraflixApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
