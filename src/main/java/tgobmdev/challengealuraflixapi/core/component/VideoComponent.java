bdd9ff2a79a3045ee35dc058bb1a10bd787dc6dd
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2023-11-29 02:01:56 -0300

    refactor: Java VideoDataBridge class divided into parts

---
+++ src/main/java/tgobmdev/challengealuraflixapi/core/component/VideoComponent.java
@@ -0,0 +1,49 @@
+package tgobmdev.challengealuraflixapi.core.component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.stereotype.Component;
+import tgobmdev.challengealuraflixapi.core.entity.VideoEntity;
+import tgobmdev.challengealuraflixapi.core.mapper.VideoMapper;
+import tgobmdev.challengealuraflixapi.core.repository.manager.VideoRepositoryManager;
+import tgobmdev.challengealuraflixapi.dto.VideoResponse;
+
+@Component
+public class VideoComponent {
+
+  private final VideoRepositoryManager videoRepositoryManager;
+  private final VideoMapper videoMapper;
+
+  public VideoComponent(VideoRepositoryManager videoRepositoryManager, VideoMapper videoMapper) {
+    this.videoRepositoryManager = videoRepositoryManager;
+    this.videoMapper = videoMapper;
+  }
+
+  public List<VideoResponse> findAllActiveVideos() {
+    return videoMapper.mapToVideoResponses(videoRepositoryManager.findAllActiveVideos());
+  }
+
+  public Optional<VideoResponse> findActiveVideoById(UUID id) {
+    return videoRepositoryManager.findActiveVideoById(id).map(videoMapper::mapToVideoResponse);
+  }
+
+  private void softDeleteVideo(VideoEntity videoEntity) {
+    videoEntity.setDeleted(true);
+    videoEntity.setDeletedAt(LocalDateTime.now());
+  }
+
+  public Optional<VideoResponse> deleteVideo(UUID id) {
+    Optional<VideoEntity> optionalVideoEntity = videoRepositoryManager.findActiveVideoById(id);
+    Optional<VideoResponse> optionalVideoResponse = Optional.empty();
+
+    if (optionalVideoEntity.isPresent()) {
+      VideoEntity videoEntity = optionalVideoEntity.get();
+      softDeleteVideo(videoEntity);
+      videoRepositoryManager.saveVideo(videoEntity);
+      optionalVideoResponse = Optional.of(videoMapper.mapToVideoResponse(videoEntity));
+    }
+    return optionalVideoResponse;
+  }
+}
