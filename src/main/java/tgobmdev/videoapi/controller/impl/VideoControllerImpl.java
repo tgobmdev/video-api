9da3824212337bf28f35e2b40bf0abd5ec67d71b
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-07-20 23:40:00 -0300

    refactor: melhoria nos logs

--- src/main/java/tgobmdev/videoapi/controller/impl/VideoControllerImpl.java
+++ src/main/java/tgobmdev/videoapi/controller/impl/VideoControllerImpl.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.UUID;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,12 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import tgobmdev.videoapi.annotation.Logging;
 import tgobmdev.videoapi.controller.VideoController;
 import tgobmdev.videoapi.dto.request.VideoRequest;
 import tgobmdev.videoapi.dto.response.VideoResponse;
 import tgobmdev.videoapi.service.VideoService;
 
-@Log4j2
 @RestController
 @RequestMapping(value = "videos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class VideoControllerImpl implements VideoController {
@@ -33,65 +32,55 @@ public class VideoControllerImpl implements VideoController {
     this.videoService = videoService;
   }
 
+  @Logging
   @Override
   @GetMapping(value = "/list")
   public ResponseEntity<List<VideoResponse>> findAllActiveVideos() {
-    log.info("Request [GET] received at [/videos]");
-    List<VideoResponse> activeVideos = videoService.findAllActiveVideos();
-    log.info("Request [GET] finished at [/videos]");
-    return ResponseEntity.ok(activeVideos);
+    return ResponseEntity.ok(videoService.findAllActiveVideos());
   }
 
+  @Logging
   @Override
   @GetMapping(value = "/{videoId}")
   public ResponseEntity<VideoResponse> findActiveVideoById(@PathVariable UUID videoId) {
-    log.info("Request [GET] received at [/videos/{}]", videoId);
-    VideoResponse activeVideo = videoService.findActiveVideoById(videoId);
-    log.info("Request [GET] finished at [/videos/{}]", videoId);
-    return ResponseEntity.ok(activeVideo);
+    return ResponseEntity.ok(videoService.findActiveVideoById(videoId));
   }
 
+  @Logging
   @Override
   @GetMapping
   public ResponseEntity<List<VideoResponse>> findAllActiveVideosByTitle(
       @RequestParam String search) {
-    log.info("Request [GET] received at [/videos?search={}]", search);
-    List<VideoResponse> activeVideosByTitle = videoService.findAllActiveVideosByTitle(search);
-    log.info("Request [GET] finished at [/videos?search={}]", search);
-    return ResponseEntity.ok(activeVideosByTitle);
+    return ResponseEntity.ok(videoService.findAllActiveVideosByTitle(search));
   }
 
+  @Logging
   @Override
   @PostMapping
   public ResponseEntity<VideoResponse> createVideo(@Valid @RequestBody VideoRequest videoRequest) {
-    log.info("Request [POST] received at [/videos]");
     VideoResponse createdVideo = videoService.createVideo(videoRequest);
 
     URI location = ServletUriComponentsBuilder.fromCurrentRequest()
         .path("/{videoId}")
         .buildAndExpand(createdVideo.id())
         .toUri();
-    log.info("Request [POST] finished at [/videos]");
     return ResponseEntity.created(location)
         .build();
   }
 
+  @Logging
   @Override
   @PatchMapping(value = "/{videoId}")
   public ResponseEntity<VideoResponse> editVideo(@PathVariable UUID videoId,
       @Valid @RequestBody VideoRequest videoRequest) {
-    log.info("Request [PATCH] received at [/videos/{}]", videoId);
-    VideoResponse editedVideo = videoService.editVideo(videoId, videoRequest);
-    log.info("Request [PATCH] finished at [/videos/{}]", videoId);
-    return ResponseEntity.ok(editedVideo);
+    return ResponseEntity.ok(videoService.editVideo(videoId, videoRequest));
   }
 
+  @Logging
   @Override
   @DeleteMapping("/{videoId}")
   public ResponseEntity<Void> deleteVideo(@PathVariable UUID videoId) {
-    log.info("Request [DELETE] received at [/videos/{}]", videoId);
     videoService.deleteVideo(videoId);
-    log.info("Request [DELETE] finished at [/videos/{}]", videoId);
     return ResponseEntity.noContent()
         .build();
   }
