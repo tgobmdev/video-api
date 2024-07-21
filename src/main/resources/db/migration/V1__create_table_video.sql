fd478844238da4154d6796400c1cd447e6ab245b
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-07-21 19:16:06 -0300

    fix: ajuste tabela videos

--- src/main/resources/db/migration/V1__create_table_video.sql
+++ src/main/resources/db/migration/V1__create_table_video.sql
@@ -1,7 +1,7 @@
 CREATE TABLE video (
 	id UUID NOT NULL,
-	description VARCHAR(255) NOT NULL,
-	title VARCHAR(255) NOT NULL,
-	url VARCHAR(255) NOT NULL,
+	description VARCHAR(255),
+	title VARCHAR(255),
+	url VARCHAR(255),
 	PRIMARY KEY (id)
 );
