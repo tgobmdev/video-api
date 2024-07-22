0ec34933c754a40beb082fbb6de1cee3253837a5
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2024-07-22 13:27:43 -0300

    feat: testes LoggingImplTest

---
+++ src/test/java/tgobmdev/videoapi/annotation/impl/LoggingImplTest.java
@@ -0,0 +1,71 @@
+package tgobmdev.videoapi.annotation.impl;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.aspectj.lang.JoinPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@ExtendWith(MockitoExtension.class)
+class LoggingImplTest {
+
+  @InjectMocks
+  private LoggingImpl loggingImpl;
+
+  @Mock
+  private HttpServletRequest request;
+
+  @Mock
+  private JoinPoint joinPoint;
+
+  @BeforeEach
+  public void setUp() {
+    ServletRequestAttributes attributes = new ServletRequestAttributes(request);
+    RequestContextHolder.setRequestAttributes(attributes);
+  }
+
+  @Test
+  void testLogBefore() {
+    when(request.getMethod()).thenReturn("GET");
+    when(request.getRequestURI()).thenReturn("/test-uri");
+    when(joinPoint.getArgs()).thenReturn(new Object[]{"param1", "param2"});
+
+    loggingImpl.logBefore(joinPoint);
+
+    verify(request, times(1)).getMethod();
+    verify(request, times(1)).getRequestURI();
+  }
+
+  @Test
+  void testLogAfterReturning() {
+    when(request.getMethod()).thenReturn("POST");
+    when(request.getRequestURI()).thenReturn("/test-uri");
+    Object result = "result";
+
+    loggingImpl.logAfterReturning(result);
+
+    verify(request, times(1)).getMethod();
+    verify(request, times(1)).getRequestURI();
+  }
+
+  @Test
+  void testLogAfterThrowing() {
+    when(request.getMethod()).thenReturn("DELETE");
+    when(request.getRequestURI()).thenReturn("/test-uri");
+    Throwable exception = new RuntimeException("Error message");
+
+    loggingImpl.logAfterThrowing(exception);
+
+    verify(request, times(1)).getMethod();
+    verify(request, times(1)).getRequestURI();
+  }
+}
